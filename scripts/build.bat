1e6042200f8c22caeb82451cb5b4ad21c54aa4be
Róbert Kiss <ert78gb@gmail.com>
2017-05-24 21:51:45 +0200

    fix builds

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,2 +1,11 @@
 npm run test
 
+if %TRAVIS_BRANCH% == "master"  (
+    if  %TRAVIS_PULL_REQUEST% == "false" (
+     npm run release;
+    ) else (
+       echo "It is not release job. Release is not created"
+    )
+ ) else (
+    echo "It is not release job. Release is not created"
+ )
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,7 @@
 yarn run test
 
-if %TRAVIS_BRANCH% == "master" && %TRAVIS_PULL_REQUEST% == "false" ] (
-    yarn run release
-)
-else (
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+     yarn run release;
+  else
     echo "It is not release job. Release is not created"
-)
+fi
