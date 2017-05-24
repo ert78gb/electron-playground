b425d4b1fee2ae6985527dbcceca7b9daaf1f353
Róbert Kiss <ert78gb@gmail.com>
2017-05-24 23:43:07 +0200

    fix dist

--- scripts/build.bat
+++ scripts/build.bat
@@ -6,7 +6,7 @@ npm run test
 
 if /I %TRAVIS_BRANCH% == "master"  (
     if /I %TRAVIS_PULL_REQUEST% == "false" (
-     npm run release;
+     npm run dist;
     ) else (
        ECHO "1. It is not release job. Release is not created"
     )
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 yarn run test
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-     yarn run release;
+     yarn run dist;
   else
     echo "It is not release job. Release is not created"
 fi
