b5705373a50621da74b61c822a320ff72c4c51bf
Róbert Kiss <ert78gb@gmail.com>
2017-05-24 20:51:50 +0200

    add build.sh file

---
+++ scripts/build.sh
@@ -0,0 +1,7 @@
+yarn run test
+
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+    yarn run release;
+  else
+    echo "It is not release job. Release is not created"
+fi
