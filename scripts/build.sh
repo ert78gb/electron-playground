6e2b772fdfbf0068ede0ee8001b0fcc7551fe907
Róbert Kiss <ert78gb@gmail.com>
2017-05-24 21:42:12 +0200

    fix build.bat script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,8 @@
 yarn run test
 
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-    yarn run release;
-  else
+if %TRAVIS_BRANCH% == "master" && %TRAVIS_PULL_REQUEST% == "false" ] (
+    yarn run release
+)
+else (
     echo "It is not release job. Release is not created"
-fi
+)
