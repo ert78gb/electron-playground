b715302c1fab4d25816222dd5de06e33767b8ff3
Róbert Kiss <ert78gb@gmail.com>
2017-05-24 23:17:42 +0200

    setlocal in build.bat

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,11 +1,17 @@
+@ECHO off
+
+SETLOCAL
+
 npm run test
 
-if %TRAVIS_BRANCH% == "master"  (
-    if  %TRAVIS_PULL_REQUEST% == "false" (
+if /I %TRAVIS_BRANCH% == "master"  (
+    if /I %TRAVIS_PULL_REQUEST% == "false" (
      npm run release;
     ) else (
-       echo "It is not release job. Release is not created"
+       ECHO "1. It is not release job. Release is not created"
     )
  ) else (
-    echo "It is not release job. Release is not created"
+    ECHO "2. It is not release job. Release is not created"
  )
+
+ENDLOCAL
