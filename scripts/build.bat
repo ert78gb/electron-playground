96d3e10f2eaf8f55aa87ee936ae5b06922a3cc4b
Róbert Kiss <ert78gb@gmail.com>
2017-05-25 00:16:34 +0200

    fix %

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,15 +1,15 @@
 @ECHO off
 
-SETLOCAL
+SETLOCAL ENABLEEXTENSIONS
 
 if /I %APPVEYOR_REPO_BRANCH% == "master"  (
-    if NOT DEFINED %APPVEYOR_PULL_REQUEST_NUMBER% (
+     if NOT DEFINED APPVEYOR_PULL_REQUEST_NUMBER (
      npm run dist
     ) else (
        ECHO "1. It is not release job. Release is not created"
     )
  ) else (
-    ECHO "2. It is not release job. Release is not created"
+    ECHO "2. It not master branch. Release is not created"
  )
 
 ENDLOCAL
