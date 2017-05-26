72d0876e3a4409f41e0c9bf6a63d35bac2ed473d
Róbert Kiss <ert78gb@gmail.com>
2017-05-26 04:25:33 +0200

    fix ENV in release.js

--- scripts/release.js
+++ scripts/release.js
@@ -5,10 +5,10 @@ let pullRequestNr = ''
 
 if (process.env.TRAVIS) {
   branchName = process.env.TRAVIS_BRANCH
-  pullRequestNr = TRAVIS_PULL_REQUEST
+  pullRequestNr = process.env.TRAVIS_PULL_REQUEST
 } else if (process.env.APPVEYOR) {
   branchName = process.env.APPVEYOR_REPO_BRANCH
-  pullRequestNr = APPVEYOR_PULL_REQUEST_NUMBER
+  pullRequestNr = process.env.APPVEYOR_PULL_REQUEST_NUMBER
 }
 
 console.log({ branchName, pullRequestNr })
